89de82d1aa6c0742c78329c489d21077d9c2719a
Nick ten Veen <nveen0930@gmail.com>
2017-11-30 17:09:28 +0100

    shuffled around files a bit

--- javascript.example/test.js
+++ javascript.example/test.js
@@ -1,1 +1,29 @@
-import _ from 'lodash';
+class Foo {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  
+  sum(){
+    return this.x + this.y;
+  }
+}
+
+class Bar extends Foo {
+  constructor(z){
+    super(42, 43);
+    this.z = z;
+  }
+  
+  betterSum(){
+    return super.sum() + this.z;
+  }
+}
+
+var bar = new Bar(123);
+console.log(bar.betterSum());
+console.log(bar.betterSum());
+console.log(bar.betterSum());
+console.log(bar.betterSum());
+
+
